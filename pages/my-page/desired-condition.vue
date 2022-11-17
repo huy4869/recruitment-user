b5beb8365c833880eef923c89f04a321dd6b26e5
hoang tuan <tuanha@amaisoft.com>
2022-11-17 14:11:53 +0700

    update page 10

---
+++ pages/my-page/desired-condition.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="cv-page">
+    <client-only>
+      <div>
+        <TitlePageElement class="show-pc"></TitlePageElement>
+        <BannerElement :banner="$t('page.my_page')" :sub-banner="$t('page.sub_web_cv')"></BannerElement>
+      </div>
+      <div class="cv-content">
+        <MenuLeftElement menu-active="desired-condition"></MenuLeftElement>
+        <DesiredConditionElement></DesiredConditionElement>
+      </div>
+    </client-only>
+  </div>
+</template>
+
+<script>
+import BannerElement from '../../components/layout/BannerElement'
+import TitlePageElement from '../../components/layout/TitlePageElement'
+import MenuLeftElement from '../../components/my-page/MenuLeftElement'
+import DesiredConditionElement from '../../components/my-page/DesiredConditionElement'
+import {
+  INDEX_SET_TITLE_MENU
+} from '../../store/store.const'
+
+export default {
+  name: 'MyPage',
+  components: {
+    TitlePageElement,
+    BannerElement,
+    MenuLeftElement,
+    DesiredConditionElement
+  },
+  middleware: 'auth',
+  created() {
+    this.$store.commit(INDEX_SET_TITLE_MENU, [
+      { name: this.$t('page.home'), route: '/' },
+      { name: this.$t('page.my_page'), route: '/my-page' }
+    ])
+  }
+}
+</script>
---
+++ pages/my-page/web-cv.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="cv-page">
+    <client-only>
+      <div>
+        <TitlePageElement class="show-pc"></TitlePageElement>
+        <BannerElement :banner="this.$t('page.web_cv')" :sub-banner="this.$t('page.sub_web_cv')"></BannerElement>
+      </div>
+      <div class="cv-content">
+        <MenuLeftElement menu-active="web-cv"></MenuLeftElement>
+        <IndexCvElement></IndexCvElement>
+      </div>
+    </client-only>
+  </div>
+</template>
+
+<script>
+import BannerElement from '../../components/layout/BannerElement'
+import TitlePageElement from '../../components/layout/TitlePageElement'
+import MenuLeftElement from '../../components/my-page/MenuLeftElement'
+import IndexCvElement from '../../components/my-page/IndexCvElement'
+import {
+  INDEX_SET_TITLE_MENU
+} from '../../store/store.const'
+
+export default {
+  name: 'MyPage',
+  components: {
+    TitlePageElement,
+    BannerElement,
+    MenuLeftElement,
+    IndexCvElement
+  },
+  middleware: 'auth',
+  created() {
+    this.$store.commit(INDEX_SET_TITLE_MENU, [
+      { name: this.$t('page.home'), route: '/' },
+      { name: this.$t('page.my_page'), route: '/my-page' }
+    ])
+  }
+}
+</script>
